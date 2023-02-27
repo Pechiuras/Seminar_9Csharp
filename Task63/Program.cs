382d9fdf4a33d103ea7918252e7e181750a37bf8
Aleksandr Pechiuras <alpechura@yandex.ru>
2023-02-27 15:31:25 +0500

    task 63 - 69, 147

---
+++ Task63/Program.cs
@@ -0,0 +1,17 @@
+﻿//Task63 Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.Clear();
+string f(int n)
+{
+  if (n == 1)
+    return "1";
+  return f(n - 1) + $", {n}";
+}
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(n));
---
+++ Task65/Program.cs
@@ -0,0 +1,15 @@
+﻿//Task65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+string f(int m, int n)
+{
+  if (n == m)
+    return $"{m}";
+  return f(m, n - 1) + $", {n}";
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(m, n));
